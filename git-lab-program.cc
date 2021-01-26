77b4b0bb1ab22500c0d6bdc984b489f300ae4e9d
Katie Clarke <kc023419@ohio.edu>
2021-01-26 15:11:02 -0500

    git lab 2

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Katie Clarke
+ *        Date: 1/26/2021
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
